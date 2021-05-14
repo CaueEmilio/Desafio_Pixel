1710fca798d161e8c9d0a8cd60cba19f1727e924
Cauê Emilio de Moraes <53952229+CaueEmilio@users.noreply.github.com>
2021-05-14 17:16:07 -0300

    Add files via upload

--- conta_pixel_verde.py
+++ conta_pixel_verde.py
@@ -5,7 +5,6 @@
 Autor: Cauê Emilio de Moraes
 Algoritmo para extração de dados de página web (link: http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar)
 Atualização: 11/05/21
-Antes da construção da contagem de pixels verdes, foram aferidos os valores das cores por meio de código
 
 '''
 
@@ -14,7 +13,8 @@ from PIL import Image
 imagem = Image.open('Syngenta.BMP','r')
 
 valor_pixel = list(imagem.getdata())
-print(valor_pixel)
+#Antes da construção da contagem de pixels verdes, foram aferidos os valores das cores por meio de um "print(valor_pixel)" neste ponto do código  
+
 print('A imagem está no formato "{0}", que é baseada em uma paleta de cores com variação entre 0 e 255, onde a cor preta possui o valor "0", a cor branca valor "255" e a cor verde valor "51"'.format(imagem.mode))
 
 verde = 0
@@ -22,4 +22,4 @@ for pixel in valor_pixel:
     if pixel == 51: #conta apenas pixels verdes
         verde += 1
 
-print(verde)
+print('\nHá {} pixels verdes na imagem'.format(verde))
